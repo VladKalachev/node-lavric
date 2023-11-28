c25c82633bb1d76d85bc9d4f2885d3de74b7c91c
Corvax <vlvete1990@yandex.ru>
2023-11-28 19:07:16 +0300

    chore: add  short polling

---
+++ lesson-2/2-short-polling/server.js
@@ -0,0 +1,72 @@
+import http from 'http';
+import { readFile } from 'fs'
+
+// todo: 1. config/app.js 2. dot env
+const DOMAIN = 'localhost';
+const PROTOCOL = 'http';
+const PORT = 3000;
+const HOST = `${PROTOCOL}://${DOMAIN}:${PORT}`; // mb variant without :port
+
+const bets = [];
+
+const server = http.createServer(function(request, response){
+	const parsedUrl = new URL(request.url, HOST);
+
+	switch(parsedUrl.pathname){
+		case '/':
+			mainPage(request, response, parsedUrl);
+			break;
+		case '/bets':
+			betsStream(request, response, parsedUrl);
+			break;
+		default:
+			response.writeHead(404);
+			response.end('Page not found');
+	}
+});
+
+server.listen(3000);
+
+function mainPage(request, response, parsedUrl){
+	readFile('./index.html', (err, buffer) => {
+		if(err !== null){
+			response.writeHead(500);
+			response.end('error');
+			// logs
+		}
+		else{
+			response.end(buffer.toString());
+		}
+	});
+}
+
+function betsStream(request, response, parsedUrl){
+	const fromId = parseInt(parsedUrl.searchParams.get('id') ?? '0');
+	let immediatelyBets = bets.filter(bet => bet.id > fromId);
+	const client = { request, response };
+	sendBetsToClient(client, immediatelyBets);
+}
+
+function sendBetsToClient(client, bets){
+	client.response.end(JSON.stringify(bets));
+}
+
+function cleanClient(response){
+	let ind = clients.findIndex(client => client.response === response);
+
+	if(ind !== -1){
+		clients.splice(ind, 1);
+	}
+}
+
+function randomBet(){
+	setTimeout(() => {
+		let id = bets.length > 0 ? bets[bets.length - 1].id : 0;
+		++id;
+		let bet = { id, value: id * 1000, time: Date.now() };
+		bets.push(bet);
+		randomBet();
+	}, 1000 * ( Math.floor(Math.random() * 20) + 20 ));
+}
+
+randomBet();
