6456a38e422bef35e8dec4499cb06eeaf80f62fc
Corvax <vlvete1990@yandex.ru>
2023-12-05 22:39:42 +0300

    chore: add stream

--- lesson-4/1-hw/0backups/1-chat-aiid=mid.js
+++ lesson-4/1-hw/0backups/1-chat-aiid=mid.js
@@ -2,22 +2,13 @@ import eventBus from '#app/globals/eventbus.js'
 import ChatAppendEvent from '#app/events/chat/append.js'
 import Message from '#app/models/message.js'
 import broadcast from '#app/globals/broadcast.js'
-import { Op } from 'sequelize'
 
 const channel = broadcast.createChannel('Chat SSE');
 eventBus.on(ChatAppendEvent.name, e => channel.forClients(client => sendMessage(client, e.message)));
 
 export default async (request, response) => {
 	let lastEventId = parseInt(request.headers['last-event-id'] ?? '0');
-
-	let immediatelyMessages = await Message.findAll({
-		where: { id: { [ Op.gt ]: lastEventId }},
-		order: [
-			['createdAt', 'DESC']
-		],
-		limit: 10
-	});
-
+	let immediatelyMessages = await Message.findAll();
 	let client = channel.addClient(request, response);
 
 	response.writeHead(200, {
@@ -25,7 +16,11 @@ export default async (request, response) => {
 		'Cache-Control': 'no-cache'
 	});
 
-	immediatelyMessages.reverse().forEach(message => sendMessage(client, message));
+	immediatelyMessages.forEach(message => {
+		if(message.id > lastEventId){
+			sendMessage(client, message);
+		}
+	});
 }
 
 function sendMessage(client, message){
