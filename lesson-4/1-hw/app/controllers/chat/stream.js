1e358b3462c6bdacd66109fabf79efda1b10fb18
Corvax <vlvete1990@yandex.ru>
2023-12-05 22:58:48 +0300

    chore: add sendEvent

--- lesson-4/1-hw/app/controllers/chat/stream.js
+++ lesson-4/1-hw/app/controllers/chat/stream.js
@@ -2,22 +2,28 @@ import eventBus from '#app/globals/eventbus.js'
 import ChatAppendEvent from '#app/events/chat/append.js'
 import Message from '#app/models/message.js'
 import broadcast from '#app/globals/broadcast.js'
-import { Op } from 'sequelize'
 
 const channel = broadcast.createChannel('Chat SSE');
-eventBus.on(ChatAppendEvent.name, e => channel.forClients(client => sendMessage(client, e.message)));
+let eventId = 0;
+const events = []; // todo in real, set max length of array
+const messagesHistoryVolume = 10;
+let emulateIdJumps = false;
+
+let messages = await Message.findAll({
+	order: [['createdAt', 'DESC']],
+	limit: messagesHistoryVolume
+});
+
+messages.reverse().forEach(message => appendEvent(message))
+
+eventBus.on(ChatAppendEvent.name, e => {
+	//emulateIdJumps = true;
+	let event = appendEvent(e.message);
+	channel.forClients(client => sendEvent(client, event));
+});
 
 export default async (request, response) => {
 	let lastEventId = parseInt(request.headers['last-event-id'] ?? '0');
-
-	let immediatelyMessages = await Message.findAll({
-		where: { id: { [ Op.gt ]: lastEventId }},
-		order: [
-			['createdAt', 'DESC']
-		],
-		limit: 10
-	});
-
 	let client = channel.addClient(request, response);
 
 	response.writeHead(200, {
@@ -25,9 +31,35 @@ export default async (request, response) => {
 		'Cache-Control': 'no-cache'
 	});
 
-	immediatelyMessages.reverse().forEach(message => sendMessage(client, message));
+	events.forEach(event => {
+		if(event.id > lastEventId){
+			sendEvent(client, event);
+		}
+	});
 }
 
-function sendMessage(client, message){
-	client.response.write(`event: append\ndata: ${JSON.stringify(message)}\nid:${message.id}\n\n`);
+function sendEvent(client, event){
+	client.response.write(`event: ${event.name}\ndata: ${JSON.stringify(event.data)}\nid:${event.id}\n\n`);
+}
+
+function appendEvent(message){
+	eventId++;
+
+	if(emulateIdJumps){
+		eventId++;
+	}
+
+	let event = {
+		id: eventId,
+		name: 'append',
+		data: message.dataValues
+	}
+
+	events.push(event);
+
+	if(events.length > messagesHistoryVolume){
+		events.shift();
+	}
+
+	return event;
 }
